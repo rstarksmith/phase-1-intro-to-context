6503648a395e1fa3bc147c80b12d9d7195a86b7e
Rachel <rstark.smith@gmail.com>
2022-05-02 10:17:11 -0500

    commit tree fc3b8a1946e087040a97a95e2c5a92d57bd8e438

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Your code here
+
+// const employee = [firstName, familyName, title, payPerHour]
+
+function createEmployeeRecord(employee){
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+    
+function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    let eventObj = {
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date
+    }
+    employee.timeInEvents.push(eventObj)
+    return employee
+}
+
+function createTimeOutEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    let eventObj = {
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date
+    }
+    employee.timeOutEvents.push(eventObj)
+    return employee
+}
+
+function hoursWorkedOnDate(employee, date){   
+    const timeIn = employee.timeInEvents.find(dateStamp => date)
+    const timeOut = employee.timeOutEvents.find(dateStamp => dateStamp.date === date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(employee, date){
+    const hours = hoursWorkedOnDate(employee, date)
+    return employee.payPerHour * hours
+}
+
+// function calculatePayroll(date) {
+
+// }
+    
+
